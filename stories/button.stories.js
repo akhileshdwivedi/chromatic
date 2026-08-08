de2c7fdc60796e664510b5269ecb0789acafc86e
Akhilesh Dwivedi <akhileshdwivedi25@gmail.com>
2026-08-08 09:52:49 +0100

    feature branch changes

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -21,13 +21,13 @@ export default {
 
 export const Primary = {
   args: {
-    label: 'Primary button Akhilesh Dwivedi',
+    label: 'Primary button Akhilesh Dwivedi feature branch',
     primary: true,
   },
 };
 
 export const Secondary = {
   args: {
-    label: 'Secondary button Swati Dwivedi',
+    label: 'Secondary button Swati Dwivedi feature branch',
   },
 };
